1820e7095bff8108b7cb982ababac655ad5c4031
Zakhar Gulchak <z.gulchak@levi9.com>
2015-05-24 19:52:55 +0300

    Implemented: Remove jQuery module usages

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,50 @@
 var url = 'http://pipes.yahoo.com/pipes/pipe.run?_id=e9a2e77dffb3205d035c4e311d77bbe6&_render=json&_callback=parseJSON';
-function parseJSON(obj) {
-    var articlesList = jQuery('<ul/>', {'class': 'articles-list'});
-    for (var key in obj.value.items) {
-        var value = obj.value.items[key];
-        var articleItem = jQuery('<li/>');
-
-        var articleMainInfo = jQuery('<div/>', {'class': 'article-main-info'});
-        var articleTitle = jQuery('<h2/>', {'text': value.title});
-        var articleImage = jQuery('<img/>', {'src': value.enclosure.url});
-        articleMainInfo.append(articleTitle).append(articleImage);
-
-        var articleAdditionalInfo = jQuery('<div/>', {'class': 'article-additional-info'});
-        var articleLink = jQuery('<div/>', {'text': 'Link: '});
-        articleLink.append(jQuery('<a/>', {'href': value.link, 'text': value.link}));
-        var articleDescription = jQuery('<div/>', {'class': 'article-description', 'text': value.description});
-        articleAdditionalInfo.append(articleDescription).append(articleLink);
-
-        articleItem.append(articleMainInfo).append(articleAdditionalInfo);
-        articlesList.append(articleItem);
-    }
-    jQuery('#container').append(articlesList);
-    jQuery('.article-additional-info').hide();
-    jQuery('.article-main-info').on('click', function(){
-        jQuery(this).next().show();
-    });
+function showAdditionalInfo(self) {
+    self.nextSibling.style.display = 'block';
 }
 
-window.onload = function(){
-    jQuery.ajax({
-        url: url,
-        dataType: 'jsonp'
-    });
-};
+function parseJSON(obj) {
+    var articlesList = document.createElement('ul');
+    articlesList.setAttribute('class', 'articles-list');
+    for (var key in obj.value.items) {
+        var value = obj.value.items[key];
+        var articleItem = document.createElement('li');
+
+        var articleMainInfo = document.createElement('div');
+        articleMainInfo.setAttribute('onclick', 'showAdditionalInfo(this)');
+        articleMainInfo.setAttribute('class', 'article-main-info');
+        var articleTitle = document.createElement('h2');
+        articleTitle.innerHTML = value.title;
+        var articleImage = document.createElement('img');
+        articleImage.setAttribute('src', value.enclosure.url);
+        articleMainInfo.appendChild(articleTitle);
+        articleMainInfo.appendChild(articleImage);
+
+        var articleAdditionalInfo = document.createElement('div');
+        articleAdditionalInfo.setAttribute('class', 'article-additional-info');
+        var articleLinkBlock = document.createElement('div');
+        articleLinkBlock.innerHTML = 'Link: ';
+        var articleLink = document.createElement('a');
+        articleLink.setAttribute('href', value.link);
+        articleLink.innerHTML = value.link;
+        articleLinkBlock.appendChild(articleLink);
+        var articleDescription = document.createElement('div');
+        articleDescription.setAttribute('class', 'article-description');
+        articleDescription.innerHTML = value.description;
+        articleAdditionalInfo.appendChild(articleDescription);
+        articleAdditionalInfo.appendChild(articleLinkBlock);
+
+        articleItem.appendChild(articleMainInfo);
+        articleItem.appendChild(articleAdditionalInfo);
+        articlesList.appendChild(articleItem);
+    }
+    document.getElementById('container').appendChild(articlesList);
+}
+
+function addScript(src) {
+    var elem = document.createElement("script");
+    elem.src = src;
+    document.head.appendChild(elem);
+}
+
+addScript(url);
